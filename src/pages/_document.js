713d9e13178c94ee3a87f6ae93a6da59cd1787c3
vitorreisler <vitorreisler@gmail.com>
2025-03-15 17:23:19 -0300

    add defer no script do flowbite

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <script src="https://unpkg.com/flowbite@1.4.1/dist/flowbite.js"></script>
+      <script defer src="https://unpkg.com/flowbite@1.4.1/dist/flowbite.js"></script>
       
       <Head />
       <body className="antialiased">
